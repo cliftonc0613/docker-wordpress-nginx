364fa930251f97e8e8351d39a1087341bf63012f
Gaetan Zoritchak <g.zoritchak@moncoachfinance.com>
2014-10-07 17:17:03 +0200

    Launch blog script.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t gzoritchak/wp-nginx image/A.
+docker build -t gzoritchak/wp-nginx image
--- startBlog.sh
+++ startBlog.sh
@@ -4,13 +4,14 @@
 #If www/www-config.php is present, there must be a dump : wordpress.dmp
 checkRepo(){
     echo Checking repo $1
-    if [ -f $1/www/www-config.php ]; and ! [ -f $1/wordpress.dmp ]; then
-       echo www/www-config.php is present but there is no database dump :: wordpress.dmp
+    echo `ls $1`
+    if [ -e $1/www/wp-config.php ] && [ ! -e $1/wordpress.dmp ]; then
+       echo www/wp-config.php is present but there is no database dump :: wordpress.dmp
        exit 1
     fi
 
-    if ! [ -f www/www-config.php ]; and [ -f wordpress.dmp ]; then
-       echo there a database dump but no config file :: www/www-config.php
+    if [ ! -e $1/www/wp-config.php ] && [ -e $1/wordpress.dmp ]; then
+       echo there a database dump but no config file :: www/wp-config.php
        exit 1
     fi
 
@@ -18,10 +19,10 @@ checkRepo(){
 
 
 updateRestoreIfBackupsYounger(){
-    if [ -f restore/wordpress.dmp ] ; and  [ -f backups/wordpress.dmp ] ; and [ backups/wordpress.dmp -nt restore/wordpress.dmp ]; then
+    if [ -e restore/wordpress.dmp ] &&  [ -e backups/wordpress.dmp ] && [ backups/wordpress.dmp -nt restore/wordpress.dmp ]; then
         rm -rf restore/previous
         mkdir restore/previous
-        mv -r restore/www restore/previous
+        mv restore/www restore/previous
         mv restore/wordpress.dmp restore/previous
         cp -r backups/www restore
         cp backups/wordpress.dmp restore
@@ -32,12 +33,11 @@ FROM=`pwd`
 cd $1
 
 checkRepo restore
-checkRepo backups
-
-updateRestoreIfBackupsYounger
-
 docker stop $1
 docker rm $1
+checkRepo backups
+updateRestoreIfBackupsYounger
+
 DOCKER_CONTAINER=$(docker run -d \
         --name=$1 \
         -P \
